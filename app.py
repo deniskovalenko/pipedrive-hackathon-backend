c715ed91d2aa18e437ade68a08c85042ef7d88ce
deniskovalenko <denis.v.kovalenko@gmail.com>
2018-05-04 13:44:06 +0300

    change port to 8080

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ if __name__ == '__main__':
     app.config['company_domain'] = sys.argv[1]
     app.config['api_token'] = sys.argv[2]
     app.config['google_maps_api_key'] = sys.argv[3]
-    app.run()
+    app.run(host='0.0.0.0',port='8080')
